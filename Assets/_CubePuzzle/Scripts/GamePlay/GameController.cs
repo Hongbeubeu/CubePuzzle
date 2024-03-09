45ee557e5784d12cb6b096bc94a4e192c7d28e85
Hongbeubeu <44913755+Hongbeubeu@users.noreply.github.com>
2024-03-09 18:21:52 +0700

    Update raycast method

--- Assets/_CubePuzzle/Scripts/GamePlay/GameController.cs
+++ Assets/_CubePuzzle/Scripts/GamePlay/GameController.cs
@@ -5,16 +5,45 @@ namespace CubePuzzle.Gameplay
 {
     public class GameController : Singleton<GameController>
     {
-        public bool IsHoldBlock;
+        public bool IsHoldBlock { get; private set; }
 
-        [SerializeField] private Transform block;
+        private Transform block;
         [SerializeField] private Transform board;
 
         public void SetBlockPositionRotation(Vector3 position)
         {
+            if (!IsHoldBlock || block == null) return;
+
             position += board.up * 2f;
             block.position = position;
             block.rotation = board.rotation;
+        }
+
+        private void Start()
+        {
+            RefreshBlock();
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Mouse1))
+            {
+                IsHoldBlock = !IsHoldBlock;
+                RefreshBlock();
+            }
+        }
+
+        private void RefreshBlock()
+        {
+            if (IsHoldBlock)
+            {
+                block = GameManager.Instance.Database.ObjectPooler.InstantiateCube().transform;
+            }
+            else if (block != null)
+            {
+                GameManager.Instance.Database.ObjectPooler.ReturnPoolCube(block.gameObject);
+                block = null;
+            }
         }
     }
 }
--- Assets/_CubePuzzle/Scripts/GamePlay/RaycastController.cs
+++ Assets/_CubePuzzle/Scripts/GamePlay/RaycastController.cs
@@ -7,22 +7,28 @@ namespace CubePuzzle.Gameplay
         [SerializeField] private Camera _mainCam;
         [SerializeField] private BoardController _boardController;
 
+        private Transform _boardTrans;
+
         private void Start()
         {
             if (_mainCam == null)
             {
                 _mainCam = Camera.main;
             }
+
+            _boardTrans ??= _boardController.transform;
         }
 
         private void Update()
         {
+            var plane = new Plane(_boardTrans.position, _boardTrans.position + _boardTrans.forward,
+                _boardTrans.position + _boardTrans.right);
             var ray = _mainCam.ScreenPointToRay(Input.mousePosition);
 
-            if (!Physics.Raycast(ray, out var hit, _mainCam.farClipPlane)) return;
+            if (!plane.Raycast(ray, out var enter)) return;
 
-            if (GameController.Instance != null) GameController.Instance.SetBlockPositionRotation(hit.point);
-            if (_boardController != null) _boardController.OnSelectTile(hit.point);
+            if (GameController.Instance != null) GameController.Instance.SetBlockPositionRotation(ray.GetPoint(enter));
+            if (_boardController != null) _boardController.OnSelectTile(ray.GetPoint(enter));
         }
     }
 }
