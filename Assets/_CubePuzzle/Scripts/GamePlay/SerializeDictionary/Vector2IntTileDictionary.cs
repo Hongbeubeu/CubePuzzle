776ba6fb2e83e16b1dc83cfc766f67f0df93b6f9
Hongbeubeu <44913755+Hongbeubeu@users.noreply.github.com>
2024-03-09 21:17:59 +0700

    Add serialize Dictionary

---
+++ Assets/_CubePuzzle/Scripts/GamePlay/SerializeDictionary/Vector2IntTileDictionary.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using UnityEngine;
+
+namespace CubePuzzle.Gameplay
+{
+    [Serializable]
+    public class Vector2IntTileDictionary : UnitySerializedDictionary<Vector2Int, Tile>
+    {
+    }
+}
