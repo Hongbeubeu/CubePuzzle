4e95791cf8219f5e0ec67c7dd943a2badae2ed43
Hongbeubeu <44913755+Hongbeubeu@users.noreply.github.com>
2024-03-17 01:31:49 +0700

    Add editor code

---
+++ Assets/CatLikeCoding/ColorPoint.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class ColorPoint
+{
+    public Color color;
+    public Vector3 position;
+}
---
+++ Assets/CatLikeCoding/EditorListOption.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+[Flags]
+public enum EditorListOption
+{
+    None = 0,
+    ListSize = 1 << 0,
+    ListLabel = 1 << 1,
+    ElementLabels = 1 << 2,
+    Buttons = 1 << 3,
+    Default = ListSize | ListLabel | ElementLabels,
+    NoElementLabels = ListSize | ListLabel,
+    All = Default | Buttons
+}
