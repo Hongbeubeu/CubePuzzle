32d6357ebd2f1180e760f90ec4dd74cddf9c73c4
Hongbeubeu <44913755+Hongbeubeu@users.noreply.github.com>
2024-03-17 13:26:52 +0700

    Refactor

---
+++ Assets/CatLikeCoding/CustomList/Editor/ListTesterInspector.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(ListTester)), CanEditMultipleObjects]
+public class ListTesterInspector : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        EditorList.Show(serializedObject.FindProperty("integers"), EditorListOption.All);
+        EditorList.Show(serializedObject.FindProperty("vectors"), EditorListOption.All);
+        EditorList.Show(serializedObject.FindProperty("colorPoints"), EditorListOption.All);
+        EditorList.Show(serializedObject.FindProperty("objects"), EditorListOption.All);
+        serializedObject.ApplyModifiedProperties();
+    }
+}
--- Assets/CatLikeCoding/CustomList/EditorList.cs
+++ Assets/CatLikeCoding/CustomList/EditorList.cs
@@ -7,26 +7,31 @@ public static class EditorList
     {
         if (!list.isArray)
         {
-            EditorGUILayout.HelpBox(list.name + "is neither an array nor a list!", MessageType.Error);
+            EditorGUILayout.HelpBox($" {list.name} is neither an array nor a list!", MessageType.Error);
             return;
         }
-        
+
         var showListLabel = (options & EditorListOption.ListLabel) != 0;
         var showListSize = (options & EditorListOption.ListSize) != 0;
+
+        var size = list.FindPropertyRelative("Array.size");
+        EditorGUILayout.BeginVertical(EditorStyles.helpBox);
         if (showListLabel)
         {
-            EditorGUILayout.PropertyField(list);
+            EditorGUILayout.BeginHorizontal();
             EditorGUI.indentLevel += 1;
+            list.isExpanded = EditorGUILayout.Foldout(list.isExpanded, list.displayName);
+            if (showListSize)
+            {
+                EditorGUILayout.LabelField(new GUIContent("length"), GUILayout.MaxWidth(60));
+                EditorGUILayout.PropertyField(size, GUIContent.none, GUILayout.MaxWidth(40));
+            }
+
+            EditorGUILayout.EndHorizontal();
         }
 
         if (!showListLabel || list.isExpanded)
         {
-            var size = list.FindPropertyRelative("Array.size");
-            if (showListSize)
-            {
-                EditorGUILayout.PropertyField(size);
-            }
-
             if (size.hasMultipleDifferentValues)
             {
                 EditorGUILayout.HelpBox("Not showing lists with different sizes.", MessageType.Info);
@@ -39,6 +44,7 @@ public static class EditorList
 
         if (showListLabel)
             EditorGUI.indentLevel -= 1;
+        EditorGUILayout.EndVertical();
     }
 
     private static GUIContent
@@ -52,11 +58,12 @@ public static class EditorList
         var showElementLabels = (options & EditorListOption.ElementLabels) != 0;
         var showButtons = (options & EditorListOption.Buttons) != 0;
 
+
         for (var i = 0; i < list.arraySize; i++)
         {
             if (showButtons)
             {
-                EditorGUILayout.BeginHorizontal();
+                EditorGUILayout.BeginHorizontal(GUI.skin.box);
             }
 
             if (showElementLabels)
@@ -74,7 +81,7 @@ public static class EditorList
                 EditorGUILayout.EndHorizontal();
             }
         }
-
+        
         if (showButtons && list.arraySize == 0 && GUILayout.Button(addButtonContent, EditorStyles.miniButton))
         {
             list.arraySize += 1;
