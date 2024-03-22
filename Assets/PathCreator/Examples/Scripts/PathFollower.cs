fb744bda80e0495604d67d83d46ec99d6a93bc41
Hongbeubeu <44913755+Hongbeubeu@users.noreply.github.com>
2024-03-22 21:19:37 +0700

    Update Vehicle can run by path

--- Assets/PathCreator/Examples/Scripts/PathFollower.cs
+++ Assets/PathCreator/Examples/Scripts/PathFollower.cs
@@ -7,12 +7,15 @@ namespace PathCreation.Examples
     // Depending on the end of path instruction, will either loop, reverse, or stop at the end of the path.
     public class PathFollower : MonoBehaviour
     {
-        [OnValueChanged(nameof(OnPathChanged))]public PathCreator pathCreator;
+        [OnValueChanged(nameof(OnPathChanged))]
+        public PathCreator pathCreator;
+
         public EndOfPathInstruction endOfPathInstruction;
         public float speed = 5;
         protected float distanceTravelled;
 
-        void Start() {
+        void Start()
+        {
             if (pathCreator != null)
             {
                 // Subscribed to the pathUpdated event so that we're notified if the path changes during the game
@@ -32,7 +35,8 @@ namespace PathCreation.Examples
 
         // If the path changes during the game, update the distance travelled so that the follower's position on the new path
         // is as close as possible to its position on the old path
-        protected void OnPathChanged() {
+        protected void OnPathChanged()
+        {
             distanceTravelled = pathCreator.path.GetClosestDistanceAlongPath(transform.position);
         }
     }
--- Assets/_RoadSystem/Scripts/Vehicle.cs
+++ Assets/_RoadSystem/Scripts/Vehicle.cs
@@ -1,26 +1,68 @@
-﻿using PathCreation.Examples;
+﻿using System.Collections.Generic;
+using DG.Tweening;
+using PathCreation.Examples;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 namespace RoadSystem
 {
     public class Vehicle : PathFollower
     {
+        [SerializeField] private List<RoadTile> path;
+
         [SerializeField] private Transform target;
+
+        private float length;
+        private int currentPathIndex;
+        private Tween currentTween;
 
         protected override void Update()
         {
-            base.Update();
-            // if (pathCreator != null)
-            // {
-            //     distanceTravelled += speed * Time.deltaTime;
-            //     var targetPosition = pathCreator.path.GetClosestDistanceAlongPath(target.position);
-            //     if (distanceTravelled >= targetPosition)
-            //     {
-            //         distanceTravelled = targetPosition;
-            //     }
-            //     transform.position = pathCreator.path.GetPointAtDistance(distanceTravelled, endOfPathInstruction);
-            //     transform.rotation = pathCreator.path.GetRotationAtDistance(distanceTravelled, endOfPathInstruction);
-            // }
+            // Do nothing
+        }
+
+        [Button]
+        private void DoMove()
+        {
+            if (path == null) return;
+            if (pathCreator == null)
+            {
+                currentPathIndex = 0;
+                distanceTravelled = 0;
+                pathCreator = path[currentPathIndex];
+            }
+            length = pathCreator.path.length;
+            var duration = (length - distanceTravelled) / speed;
+            var fromValue = distanceTravelled;
+            currentTween?.Kill();
+            currentTween = DOVirtual.Float(fromValue, length, duration, value =>
+            {
+                distanceTravelled = value;
+                transform.position = pathCreator.path.GetPointAtDistance(distanceTravelled, endOfPathInstruction);
+                transform.rotation = pathCreator.path.GetRotationAtDistance(distanceTravelled, endOfPathInstruction);
+            }).SetEase(Ease.Linear).OnComplete(NextPath);
+        }
+        
+        private void NextPath()
+        {
+            currentPathIndex++;
+            currentPathIndex %= path.Count;
+            pathCreator = path[currentPathIndex];
+            distanceTravelled = 0;
+            length = pathCreator.path.length;
+            DoMove();
+        }
+
+        [Button]
+        private void Pause()
+        {
+            currentTween?.Pause();
+        }
+
+        [Button]
+        private void Resume()
+        {
+            currentTween?.Play();
         }
     }
 }
